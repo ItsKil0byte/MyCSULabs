0492fdb92d7c87d44d21d964f6fd10194567bf97
Alexander Lebedev <itskilobyte@gmail.com>
2023-11-20 12:05:19 +0500

    remade for second lab

--- SimpleCalculatorLabs/Program.cs
+++ SimpleCalculatorLabs/Program.cs
@@ -2,20 +2,15 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Parse(List<double> numbers, List<char> operations, string input)
         {
-            Console.Write("Введите математическое выражение: ");
-            string input = Console.ReadLine().Replace(" ", "");
-            List<string> numbers = new List<string>();
-            List<char> operations = new List<char>();
-
             string number = "";
 
             for (int i = 0; i < input.Length; i++)
             {
-                if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/')
+                if (!char.IsDigit(input[i]))
                 {
-                    numbers.Add(number);
+                    numbers.Add(double.Parse(number));
                     operations.Add(input[i]);
                     number = "";
                 }
@@ -25,10 +20,69 @@
                 }
             }
 
-            numbers.Add(number);
+            numbers.Add(double.Parse(number));
+        }
+        public static double GetNumber(double number1, double number2, char operation)
+        {
+            switch (operation)
+            {
+                case '+': return number1 + number2;
+                case '-': return number1 - number2;
+                case '*': return number1 * number2;
+                case '/': return number1 / number2;
+                default: return 0;
+            }
+        }
+        public static void Calculate(List<double> numbers, List<char> operations)
+        {
+            while (operations.Count > 0)
+            {
+                if (operations.Contains('*') || operations.Contains('/'))
+                {
+                    int multiplicationIndex = operations.IndexOf('*');
+                    int divisionIndex = operations.IndexOf('/');
+                    int currentOperationIndex;
 
-            Console.WriteLine($"Числа: {string.Join(" ", numbers)}");
-            Console.WriteLine($"Операторы: {string.Join(" ", operations)}");
+                    if (multiplicationIndex == -1) currentOperationIndex = divisionIndex;
+                    else if (divisionIndex == -1) currentOperationIndex = multiplicationIndex;
+                    else currentOperationIndex = Math.Min(divisionIndex, multiplicationIndex);
+
+                    double firstNumber = numbers[currentOperationIndex];
+                    double secondNumber = numbers[currentOperationIndex + 1];
+                    char operation = operations[currentOperationIndex];
+
+                    double newNumber = GetNumber(firstNumber, secondNumber, operation);
+
+                    numbers.RemoveAt(currentOperationIndex + 1);
+                    numbers.RemoveAt(currentOperationIndex);
+                    operations.RemoveAt(currentOperationIndex);
+
+                    numbers.Insert(currentOperationIndex, newNumber);
+                }
+                else if (operations.Contains('+') || operations.Contains('-'))
+                {
+                    double newNumber = GetNumber(numbers[0], numbers[1], operations[0]);
+
+                    numbers.RemoveAt(1);
+                    numbers.RemoveAt(0);
+                    operations.RemoveAt(0);
+
+                    numbers.Insert(0, newNumber);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите математическое выражние: ");
+            string input = Console.ReadLine().Replace(" ", "");
+
+            List<double> numbers = new List<double>();
+            List<char> operations = new List<char>();
+
+            Parse(numbers, operations, input);
+            Calculate(numbers, operations);
+
+            Console.WriteLine($"Результат - {string.Join(" ", numbers)}");
             Console.ReadLine();
         }
     }
